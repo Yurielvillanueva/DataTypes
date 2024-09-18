84c81387ad0a243bb24dd7cb1c31064f2dadd5b8
Tempest <107783416+Yurielvillanueva@users.noreply.github.com>
2024-09-18 13:38:22 +0800

    Add files via upload

---
+++ Prelim.java
@@ -0,0 +1,29 @@
+public class Prelim {
+
+    public static void main(String[] Ralph) {
+        int x = 10, y= 5; //This line is a declaration and initialization of two variables
+
+        System.out.println();
+
+        System.out.println("Integer Operations:"); // This line is for integer operations or int operations
+        System.out.println("Addition: 10 + 5 ="+x+y); // it's add 10 and 5 together
+        System.out.println("Subtraction: 10 - 5 = "+(x-y)); // it's subtract 5 from 10
+        System.out.println("Multiplication: 10 * 5 = "+x*y); // it's multiply 10 by 5
+        System.out.println("Division: 10 / 5 = "+x/y); // it's divide 10 by 5
+
+        System.out.println(); // this line of code is used to seperate the outputs
+
+        System.out.println("Double Operations:"); // This line is for double operations or double
+        double a = 15.5, b = 4.5;
+        System.out.println("Addition: 15.5 + 4.5 = "+a+b); // it's add 15.5 and 4.5 together
+        System.out.println("Subtraction: 15.5 - 4.5 = "+(a-b)); // it's subtract 4.5 from 15.5
+        System.out.println("Multiplication: 15.5 * 4.5 = "+a*b); // it's multiply 15.5 by 4.5
+        System.out.println("Division: 15.5 / 4.5 = "+a/b); // it's divide 15.5 by 4.5
+
+        System.out.println(); // this line of code is used to seperate the outputs
+
+        System.out.println("OUTPUT FOR STRINGS:");
+        String  str1 = "Hello", str2 = "World";
+        System.out.println(str1 + str2);// it's concatenate the two strings
+    }
+}
